1c603160492399ccb9bd151f7ddee41bd2d8ea11
FRobaglia <felix.robaglia@gmail.com>
2020-05-23 18:34:38 +0200

    added title styled component with storybook implementation

---
+++ src/stories/0-Title.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Title, { colors, types } from '../ui/atoms/Title';
+import { withKnobs, text } from "@storybook/addon-knobs";
+import './../assets/css/main.css'
+
+export default {
+    title: 'Title',
+    component: Title,
+    decorators: [withKnobs]
+};
+
+export const Main = () => <Title text={text("Title", "Minecraft")} type={types.main} color={colors.white}/>;
+export const MainHighlight = () => <Title text={text("Title", "Minecraft")} type={types.main} color={colors.purple}/>;
+
+export const Nav = () => <Title text={text("Title", "Parcourir",)} type={types.navigation} color={colors.white}/>;
+export const NavOnHover = () => <Title text={text("Title", "Parcourir")} type={types.navigation} color={colors.purple}/>;
